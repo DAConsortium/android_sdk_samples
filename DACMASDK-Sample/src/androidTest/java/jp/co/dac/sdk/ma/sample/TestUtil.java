0bd560c6369eb7c2cbf161b5da0aaeb9e56910be
SatoShun <shun.sato1@gmail.com>
2016-03-14 08:52:40 +0000

    Add ScenarioTests

--- DACMASDK-Sample/src/androidTest/java/jp/co/dac/sdk/ma/sample/TestUtil.java
+++ DACMASDK-Sample/src/androidTest/java/jp/co/dac/sdk/ma/sample/TestUtil.java
@@ -13,10 +13,34 @@ import jp.co.dac.ma.sdk.widget.DACVideoPlayerView;
 import jp.co.dac.ma.sdk.widget.player.VideoPlayer;
 
 public final class TestUtil {
-    public static void waitPlayer(DACVideoPlayerView videoPlayerView) throws Exception {
+
+    public static void waitPlayerUntilPlayed(DACVideoPlayerView videoPlayerView) throws Exception {
         while (true) {
             VideoPlayer player = videoPlayerView.getVideoPlayer();
             if (player != null && player.isPlaying()) break;
+            Thread.sleep(50);
+        }
+    }
+
+    public static void waitPlayerUntilNextPlayed(DACVideoPlayerView videoPlayerView) throws Exception {
+        int previousPosition = videoPlayerView.getVideoPlayer().getCurrentPosition();
+
+        while (true) {
+            VideoPlayer player = videoPlayerView.getVideoPlayer();
+            if (player != null && player.isPlaying()) {
+                if (previousPosition > player.getCurrentPosition()) {
+                    break;
+                }
+                previousPosition = player.getCurrentPosition();
+            }
+            Thread.sleep(50);
+        }
+    }
+
+    public static void waitPlayerUntilPaused(DACVideoPlayerView videoPlayerView) throws Exception {
+        while (true) {
+            VideoPlayer player = videoPlayerView.getVideoPlayer();
+            if (player == null || !player.isPlaying()) break;
             Thread.sleep(50);
         }
     }
@@ -30,6 +54,13 @@ public final class TestUtil {
         } else {
             // TODO
         }
+    }
 
+    public static VideoPlayer getAdVideoPlayer(Activity activity) {
+        return ((DACVideoPlayerView) activity.findViewById(R.id.ad_video_player)).getVideoPlayer();
+    }
+
+    public static VideoPlayer getContentVideoPlayer(Activity activity) {
+        return ((DACVideoPlayerView) activity.findViewById(R.id.content_video_player)).getVideoPlayer();
     }
 }
