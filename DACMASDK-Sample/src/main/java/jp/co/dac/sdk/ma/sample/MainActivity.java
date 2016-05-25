c367773219c6192586b08c6243c7682eb59552b4
SatoShun <shun.sato1@gmail.com>
2016-05-25 07:52:03 +0000

    do not use jack cuz this is a experimental

--- DACMASDK-Sample/src/main/java/jp/co/dac/sdk/ma/sample/MainActivity.java
+++ DACMASDK-Sample/src/main/java/jp/co/dac/sdk/ma/sample/MainActivity.java
@@ -2,6 +2,7 @@ package jp.co.dac.sdk.ma.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import jp.co.dac.sdk.ma.sample.adpod.AdpodActivity;
 import jp.co.dac.sdk.ma.sample.content.ContentActivity;
@@ -16,10 +17,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(bundle);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.adpod).setOnClickListener(view -> startActivity(AdpodActivity.getCallingIntent(this)));
-        findViewById(R.id.vertical).setOnClickListener(view -> startActivity(VerticalActivity.getCallingIntent(this)));
-        findViewById(R.id.content).setOnClickListener(view -> startActivity(ContentActivity.getCallingIntent(this)));
-        findViewById(R.id.no_content).setOnClickListener(view -> startActivity(NoContentActivity.getCallingIntent(this)));
-        findViewById(R.id.vmap).setOnClickListener(view -> startActivity(VmapActivity.getCallingIntent(this)));
+        findViewById(R.id.adpod).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(AdpodActivity.getCallingIntent(MainActivity.this));
+            }
+        });
+
+        findViewById(R.id.vertical).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(VerticalActivity.getCallingIntent(MainActivity.this));
+            }
+        });
+
+        findViewById(R.id.content).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(ContentActivity.getCallingIntent(MainActivity.this));
+            }
+        });
+
+        findViewById(R.id.no_content).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(NoContentActivity.getCallingIntent(MainActivity.this));
+            }
+        });
+
+        findViewById(R.id.vmap).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(VmapActivity.getCallingIntent(MainActivity.this));
+            }
+        });
     }
 }
