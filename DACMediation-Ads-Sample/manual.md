5c4011b589948d11098d15afcd3dff569c74056d
SatoShun <shun.sato1@gmail.com>
2016-06-21 05:34:46 +0000

    Add sample

---
+++ DACMediation-Ads-Sample/manual.md
@@ -0,0 +1,67 @@
+# Mediation + Facebook Audience Networkマニュアル
+
+Android Studio + Gradleを使うことを想定しています.
+
+
+## 事前準備
+
+- Android Studio 1.5以上
+
+
+## 組み込み
+
+### ライブラリの組み込み
+
+build.gradleにdependenciesを追加します.
+
+
+```gradle
+repositories {
+    maven {
+        url 'https://raw.githubusercontent.com/DAConsortium/android-sdk/master/'
+    }
+}
+
+dependencies {
+    compile 'jp.co.dac:dac-mediation-sdk:${latest sdk version}'
+    compile 'jp.co.dac:dac-mediation-sdk-adapter:${latest sdk version}'
+}
+```
+
+
+## 実装
+
+まず, MediationViewを生成します. これは, メディエーションを開始するための基本クラスになります.
+
+```java
+MediationView mediationView = new MediationView(this);
+mediationView.setPlacementId("your dac placement id", "ad height", "ad witth");
+```
+
+
+次に, `FacebookRotateHandler`を生成します.
+これは, Facebook Audience NetworkのAdViewを生成するために必要になります.
+
+ここでのAdSizeは, メディエーションのサイズと合わせて下さい.
+例えば, 幅320, 高さ50の広告の場合は, `AdSize.BANNER_320_50`を使って下さい.
+
+```java
+FacebookRotateHandler fbRotateHandler = new FacebookRotateHandler.Builder(
+        binding.adViewContainer, "your facebook id", AdSize.BANNER_320_50)
+        .build();
+
+mediationView.addRotateHandler(fbRotateHandler);
+```
+
+最後に, 生成したMediationViewを表示したい箇所に`ViewGroup.addView`して下さい.
+
+```java
+view.addView(mediationView);
+```
+
+より詳しい実装は, サンプルの`MainActivity.java`を参照して下さい.
+
+
+## 確認
+
+広告が表示されていることを確認して下さい.
---
+++ DACMediation-Ads-Sample/src/main/java/jp/co/dac/sdk/audience/network/sample/MainActivity.java
@@ -0,0 +1,58 @@
+package jp.co.dac.sdk.audience.network.sample;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.ads.AdSettings;
+import com.facebook.ads.AdSize;
+
+import jp.co.dac.dacadssdk.MediationView;
+import jp.co.dac.dacadssdk.ads.FacebookRotateHandler;
+import jp.co.dac.sdk.audience.network.sample.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int DAC_PLACEMENT_ID = 0; // input your placement id
+    private static final String FACEBOOK_ID = "your facebook id";
+
+    private ActivityMainBinding binding;
+
+    private MediationView mediationView;
+    private FacebookRotateHandler fbRotateHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        prepareFacebookAd();
+        populateMediationView();
+    }
+
+    private void prepareFacebookAd() {
+        fbRotateHandler = new FacebookRotateHandler.Builder(binding.adViewContainer,
+                FACEBOOK_ID, AdSize.BANNER_320_50)
+                .build();
+    }
+
+    private void populateMediationView() {
+        mediationView = new MediationView(this);
+        mediationView.setPlacementId(DAC_PLACEMENT_ID, 50, 320);
+
+        mediationView
+                .addRotateHandler(fbRotateHandler);
+
+        binding.adViewContainer.addView(mediationView);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        fbRotateHandler.release();
+        mediationView
+                .removeRotateHandler(fbRotateHandler);
+    }
+}
