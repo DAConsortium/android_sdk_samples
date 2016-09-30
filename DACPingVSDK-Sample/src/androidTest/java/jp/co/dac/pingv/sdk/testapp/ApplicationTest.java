8bd7a3a2baa9682aa1dfbc2fefc3bbd3700d6804
SatoShun <shun.sato1@gmail.com>
2016-09-30 10:28:35 +0000

    new pingv

---
+++ DACPingVSDK-Sample/src/androidTest/java/jp/co/dac/pingv/sdk/testapp/ApplicationTest.java
@@ -0,0 +1,10 @@
+package jp.co.dac.pingv.sdk.testapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
